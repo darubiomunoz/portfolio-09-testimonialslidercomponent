61bcb45f0af99be7276c4e3d14bd09efc32c31af
Diego Rubio <darubiomunoz@gmail.com>
2021-03-08 10:04:25 -0300

    texts styles / component for mmobile devices done

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -28,8 +28,9 @@ const Testimonials = () => {
                     </div>
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”</p>
+                    <p className="card-name">John Tarkpor</p>
+                    <p className="card-occupation">Junior Front-end Developer</p>
                 </div>
             </div>
         </div>
