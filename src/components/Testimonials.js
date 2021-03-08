15b6dd1b09cc1f5340231df09d4ab9b9c5cc2eae
Diego Rubio <darubiomunoz@gmail.com>
2021-03-08 07:47:44 -0300

    background styles

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './styles/Testimonials.css';
 
-const Testimonials = () => {
-    returm (
-        <div >
+import imageJohn from '../assets/images/image-john.jpg';
+import imageTanya from '../assets/images/image-tanya.jpg';
+import pattern1 from '../assets/images/pattern-bg.svg';
+import pattern2 from '../assets/images/pattern-curve.svg';
+import pattern3 from '../assets/images/pattern-quotes.svg';
 
+const Testimonials = () => {
+    return (
+        <div className="testimonials">
+            <img src={pattern1} className="pattern1" alt="..." />
+            <img src={pattern2} className="pattern2" alt="..." />
+            <img src={pattern3} className="pattern3" alt="..." />
+            <div className="card">
+                <img src=""/* {imageJohn} */ className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     );
 }
