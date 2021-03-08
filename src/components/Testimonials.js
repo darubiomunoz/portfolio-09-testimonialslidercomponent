2e4627ada9365b7da763065102b20031ec5e699c
Diego Rubio <darubiomunoz@gmail.com>
2021-03-08 08:23:48 -0300

    image layout styles

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,15 +10,16 @@ import pattern3 from '../assets/images/pattern-quotes.svg';
 const Testimonials = () => {
     return (
         <div className="testimonials">
-            <img src={pattern1} className="pattern1" alt="..." />
-            <img src={pattern2} className="pattern2" alt="..." />
-            <img src={pattern3} className="pattern3" alt="..." />
+            <img src={pattern1} className="pattern1" alt="background pattern" aria-hidden="true" />
+            <img src={pattern2} className="pattern2" alt="background pattern" aria-hidden="true" />
+            <img src={pattern3} className="pattern3" alt="background pattern" aria-hidden="true" />
             <div className="card">
-                <img src=""/* {imageJohn} */ className="card-img-top" alt="..." />
+                <div className="card-container">
+                    <img src= {imageJohn} className="card-img-top" alt="Profile photograph" />
+                </div>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
