5949311260f74f5bd6dddd072c945b80d5e382ba
Diego Rubio <darubiomunoz@gmail.com>
2021-03-14 00:56:07 -0300

    imprtoving logic (bug to fix yet)

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/Testimonials.css';
 
 import data from '../assets/data.json';
@@ -9,34 +9,25 @@ import arrowLeft from '../assets/images/icon-prev.svg';
 import arrowRight from '../assets/images/icon-next.svg';
 
 const ComponentLogic = () => {
-    const [ visible, setVisible ] = useState('testimonial1');
+    const [ click, setClick ] = useState(false);
+    const [ id, setId ] = useState([0]);
     
     const handleClick = () => {
-        const testimonials = document.querySelector('.testimonials');
-        const testimonial1 = testimonials.childNodes[3];
-        const testimonial2 = testimonials.childNodes[4];
-        
-        const showTestimonial1 = () => {
-            setVisible('testimonial1');
-            testimonial1.classList.remove('invisible');
-            testimonial2.classList.add('invisible');
-        }
-
-        const showTestimonial2 = () => {
-            setVisible('testimonial2');
-            testimonial1.classList.add('invisible');
-            testimonial2.classList.remove('invisible');
-        }
-
-        if(visible === 'testimonial1') showTestimonial2();
-        else showTestimonial1();
+        debugger;
+        if(!click) setClick(true);
+        else setClick(false);
     }
 
-    return { handleClick };
+    useEffect(() => {
+        if(id.includes(0)) setId([1]);
+        else setId([0]);
+    }, [click])
+
+    return { handleClick, id, click };
 }
 
 const Testimonials = () => {
-    const { handleClick } = ComponentLogic();
+    const { handleClick, id, click } = ComponentLogic();
 
     return (
         <div className="testimonials">
@@ -45,7 +36,7 @@ const Testimonials = () => {
             <img src={pattern3} className="pattern3" alt="background pattern" aria-hidden="true" loading="lazy" />
             {data.map(data => {
                 return (
-                    <div key={data.id} className={data.className}>
+                    <div key={data.id} className={`card ${(data.id === '0'? '' : 'invisible') || (id.includes(0) ? 'invisible' : '')}`}>
                         <div className="card-container" >
                             <img src={data.image} className="card-img-top" alt="Profile photograph" loading="lazy" />
                             <div className="btn-group" role="group" aria-label="Basic example">
